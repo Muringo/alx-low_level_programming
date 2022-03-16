b897558d6c80f75a75ce142da4a58b8801d4cbd1
Muringo <muringolucie@gmail.com>
2022-03-16 06:15:30 -0700

    Low level programming variables task 0

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -3,7 +3,7 @@
 #include <stdio.h>
 
 /**
- *main - Entry point
+ * main - Entry point
  *
  * Return: Always 0 (Success/correct)
  */
@@ -21,12 +21,13 @@ printf("%i is zero\n", n);
 }
 
 else if (n<0)
-{printf(%i is negative\n", n);
+{
+printf("%i is negative\n", n);
 }
 
 else
 {
-printf(%i is positive\n", n);
+printf("%i is positive\n", n);
 }
 
 return (0);
