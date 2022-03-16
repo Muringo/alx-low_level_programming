506cb4ba4b8a7dce2026d3888f98c5127b02104e
Muringo <muringolucie@gmail.com>
2022-03-16 14:11:46 -0700

    Low level programming variables task 2

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -2,13 +2,13 @@
 /**
  * main - Entry point
  *
- *Return: Always 0 (Success/correct)
+ * Return: Always 0 (Success/correct)
  */
 int main(void)
 {
 char alphabet;
 
-for (alphabet = 'a'; alphabet <= z; alphabet++)
+for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
 {
 putchar(alphabet);
 }
